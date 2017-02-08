8d48e14cbeac1a780c47bda043e4d2f0928810b1
wha <wha@sourcepole.ch>
2017-02-08 13:21:56 +0100

    Simplified uriDatabaseConnect function

--- connector.py
+++ connector.py
@@ -9,16 +9,8 @@ class Connector:
         pass
 
     def uriDatabaseConnect(self, uri):
-        layer_table = uri.table()
-        layer_db = uri.database()
-        layer_schema = uri.schema()
-        layer_host = uri.host()
-        layer_user = uri.username()
-        layer_password = uri.password()
-        layer_port = uri.port()
-
         try:
-            conn_string = "dbname=%s user=%s host='%s' password=%s" % (layer_db, layer_user, layer_host, layer_password)
+            conn_string = "dbname=%s user=%s host='%s' password=%s" % (uri.database(), uri.username(), uri.host(), uri.password())
             conn = psycopg2.connect(conn_string)
             cur = conn.cursor()
             return cur
