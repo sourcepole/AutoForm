ea87a926173edac88d8a41a525ecef43e11414bf
wha <wha@sourcepole.ch>
2017-02-09 13:34:17 +0100

    Added header comment to .py files

--- autoform.py
+++ autoform.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    copyright            : (C) 2017 by William Habelt / Sourcepole AG
+#    email                : wha@sourcepole.ch
+
+
 from qgis.core import QgsFeature,  QgsMapLayerRegistry
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
