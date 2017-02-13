60d3b5fba9ed87a83d39907a66f9a382e3fa2c38
wha <wha@sourcepole.ch>
2017-02-13 11:30:36 +0100

    Improved the connector class logic based on vector.py from the tools plugin

--- connector.py
+++ connector.py
@@ -11,6 +11,7 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from qgis.core import *
 
 import psycopg2
 
@@ -23,11 +24,27 @@ class Connector:
 
     def uriDatabaseConnect(self, uri):
         """Create a connection from a uri and return a cursor of it."""
-        try:
-            conn_string = "dbname=%s user=%s host='%s' password=%s" % (uri.database(), uri.username(), uri.host(), uri.password())
-            conn = psycopg2.connect(conn_string)
-            cur = conn.cursor()
-            return cur
-        except:
-            QMessageBox.warning(self.iface.mainWindow(), "Connection Error", "Failed to connect to database. Please make sure that your connection information is correct.")
-            return False
+        conninfo = uri.connectionInfo()
+        conn = None
+        ok = False
+        while not conn:
+            try:
+                conn = psycopg2.connect(uri.connectionInfo())
+                cur = conn.cursor()
+            except psycopg2.OperationalError as e:
+                (ok, user, passwd) = QgsCredentials.instance().get(conninfo, uri.username(), uri.password())
+                if not ok:
+                    break
+
+                uri.setUsername(user)
+                uri.setPassword(passwd)
+
+        if not conn:
+            QMessageBox.warning(self.iface.mainWindow(), "Connection Error", "Could not connect to PostgreSQL database - check connection info")
+
+        if ok:
+            QgsCredentials.instance().put(conninfo, user, passwd)
+
+        ogrstr = "PG:%s" % uri.connectionInfo()
+
+        return cur
